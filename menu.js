aa3e0c2fe6f49384d57065653cb098b37d0e3077
Casey Geist <tonewardbound@gmail.com>
2022-04-28 15:02:52 -0400

    menu outline

---
+++ menu.js
@@ -0,0 +1,30 @@
+// Create a menu app as seen in this week’s video. 
+// What you create is up to you as long as it meets the following requirements.
+// 	•	Use at least one array.
+// 	•	Use at least two classes.
+// 	•	Your menu should have the options to create, view, and delete elements.
+
+
+/*
+MENU
+=====
+1) create character
+    enter name for your new character: 
+    enter points for Power, Toughness, Intellect and Charisma, totaling up to 15:
+    e.g. 5, 4, 4, 2
+    >total points exceed 15, try again
+    >character <name> created
+        add character to array
+
+2) view characters
+    list all characters in array
+
+3) delete character
+    enter ID/name of character to delete:
+    >no character with that name/ID, try again
+    >character deleted
+
+4) exit
+
+*/
+
